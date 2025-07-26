3316bffd74d355ec939d064be501f55b4608b20f
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-07-26 22:05:16 +0600

    Organized Schema and Added Model

--- src/app/modules/product.model.ts
+++ src/app/modules/product.model.ts
@@ -1,5 +1,21 @@
 import { Schema, model, connect } from 'mongoose';
-import { Product } from './product/product.interface';
+import { Inventory, Product, Product, Product, Variant } from './product/product.interface';
+
+// Creating schema
+const VariantSchema = new Schema<Variant>(
+    {
+        type: { type: String, required: true },
+        value: { type: String, required: true },
+    },
+    { _id: false }); //jate etar jonno alada mongodb te id create na hoy
+
+
+const inventorySchema = new Schema<Inventory>({
+
+    quantity: { type: Number, required: true },
+    inStock: { type: Boolean, required: true },
+
+}, { _id: false }))
 
 const productSchema = new Schema<Product>({
     name: { type: String, required: true },
@@ -7,21 +23,9 @@ const productSchema = new Schema<Product>({
     price: { type: Number, required: true },
     category: { type: String, required: true },
     tags: { type: [String], default: [] },
-    variants: [
-        {
-            type: {
-                type: String,
-                required: true,
-            },
-            value: {
-                type: String,
-                required: true,
-            },
-        },
-    ],
-    inventory: {
-        quantity: { type: Number, required: true },
-        inStock: { type: Boolean, required: true },
-    },
+    variants: [VariantSchema],
+    inventory: inventorySchema,
 });
 
+// Creating Model
+const Product = model<Product>('Product', productSchema);
