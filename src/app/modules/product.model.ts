5b5dd67952fd5f40202a4746a685bd41f3df71da
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-07-31 18:10:14 +0600

    Implemented Validation

--- src/app/modules/product.model.ts
+++ src/app/modules/product.model.ts
@@ -4,28 +4,64 @@ import { Inventory, Product, Variant } from './product/product.interface';
 // Creating schema
 const VariantSchema = new Schema<Variant>(
   {
-    type: { type: String, required: true },
-    value: { type: String, required: true },
+    type: {
+      type: String,
+      required: [true, 'Variant type is required.'],
+      trim: true,
+    },
+    value: {
+      type: String,
+      required: [true, 'Variant value is required.'],
+      trim: true,
+    },
   },
   { _id: false },
-); //jate etar jonno alada mongodb te id create na hoy
+);
+//jate etar jonno alada mongodb te id create na hoy
 
 const inventorySchema = new Schema<Inventory>(
   {
-    quantity: { type: Number, required: true },
-    inStock: { type: Boolean, required: true },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    inStock: {
+      type: Boolean,
+      required: true,
+    },
   },
   { _id: false },
 );
 
 const productSchema = new Schema<Product>({
-  name: { type: String, required: true },
+  name: {
+    type: String,
+    required: [true, 'Product name is required.'],
+    trim: true,
+    validate: {
+      validator: function (value: string) {
+        const nameStr = value.charAt(0).toUpperCase() + value.slice(1);
+        return nameStr === value;
+      },
+      message: '{VALUE} is not in capitalize format',
+    },
+  },
   description: { type: String },
-  price: { type: Number, required: true },
-  category: { type: String, required: true },
+  price: { type: Number, required: [true, 'Product price is required.'] },
+  category: {
+    type: String,
+    required: [true, 'Product category is required.'],
+    trim: true,
+  },
   tags: { type: [String], default: [] },
-  variants: [VariantSchema],
-  inventory: inventorySchema,
+  variants: {
+    type: [VariantSchema],
+    required: [true, 'At least one product variant is required.'],
+  },
+  inventory: {
+    type: inventorySchema,
+    required: [true, 'Product inventory information is required.'],
+  },
 });
 
 // Creating Model
--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -14,46 +14,49 @@ const createProduct = async (req: Request, res: Response) => {
       data: result,
     });
   } catch (err) {
-    console.log(err);
-  }
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong !',
+      error: err,
+    }
 };
 
-// Getting all the products
-const getAllProducts = async (req: Request, res: Response) => {
-  try {
-    const result = await ProductServices.getAllProductsFromDB();
+  // Getting all the products
+  const getAllProducts = async (req: Request, res: Response) => {
+    try {
+      const result = await ProductServices.getAllProductsFromDB();
 
-    // Send Response
-    res.status(200).json({
-      success: true,
-      message: 'Product is Retrieved  Successfully',
-      data: result,
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
+      // Send Response
+      res.status(200).json({
+        success: true,
+        message: 'Product is Retrieved  Successfully',
+        data: result,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-// getting only one Product by name
-const getASingleProduct = async (req: Request, res: Response) => {
-  try {
-    const { productName } = req.params;
-    const result = await ProductServices.getASingleProductFromDB(productName);
+  // getting only one Product by name
+  const getASingleProduct = async (req: Request, res: Response) => {
+    try {
+      const { productName } = req.params;
+      const result = await ProductServices.getASingleProductFromDB(productName);
 
-    // sending Response
-    res.status(200).json({
-      success: true,
-      message: 'Your Product Retrieved Successfully ',
-      data: result,
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
+      // sending Response
+      res.status(200).json({
+        success: true,
+        message: 'Your Product Retrieved Successfully ',
+        data: result,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-// So that amra Route e use korte pari
-export const ProductControllers = {
-  createProduct,
-  getAllProducts,
-  getASingleProduct,
-};
+  // So that amra Route e use korte pari
+  export const ProductControllers = {
+    createProduct,
+    getAllProducts,
+    getASingleProduct,
+  };
